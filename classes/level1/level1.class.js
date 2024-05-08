9cd739e054a0d4f8d7aecc905ae867b93c366fa6
Your Name <you@example.com>
2024-05-08 13:46:33 +0200

    cleaned class Level1

--- classes/level1/level1.class.js
+++ classes/level1/level1.class.js
@@ -1,120 +1,71 @@
 class Level1 {
 
 
-    // set 64 values + translation!!!
-
-
+    // jsdoc
     constructor() {
-        this.loadSection1();
-        this.loadSection2();
-        this.loadSection3();
-        this.loadSection4();
-        this.loadSection5();
-        this.loadSection6();
-        this.loadSection7();
-        this.loadSection8();
+        this.loadSection(Section1, 0);
+        this.loadSection(Section2, 1);
+        this.loadSection(Section3, 2);
+        this.loadSection(Section4, 3);
+        this.loadSection(Section5, 4);
+        this.loadSection(Section6, 5);
+        this.loadSection(Section7, 6);
+        this.loadSection(Section8, 7);
     }
 
 
-    loadSection1() {
-        let section = new Section1();
+    // jsdoc
+    loadSection(Section, t) {
+        let section = this.getSection(Section);
+        this.loadSectionObjects(section, t);
+    }
+
+
+    // jsdoc
+    getSection(Section) {
+        return new Section();
+    }
+
+
+    // jsdoc
+    loadSectionObjects(section, t) {
         for (const [key] of Object.entries(section)) {
-            this.setObject(key);
-            for (let i = 0; i < section[key].length; i++) {
-                let object = section[key][i];
-                this[key].push(object);
-            }
+            this.setObjectGroup(key);
+            this.loadObjectGroup(section, t, key);
         }
     }
 
 
-    setObject(key) {
-        if (this[key] === undefined) {
-            this[key] = [];
+    // jsdoc
+    setObjectGroup(key) {
+        this[key] = (this[key]) ? this[key] : [];
+    }
+
+
+    // jsdoc
+    loadObjectGroup(section, t, key) {
+        for (let i = 0; i < section[key].length; i++) {
+            let object = this.getObject(section, key, i);
+            this.loadObject(t, key, object);
         }
     }
 
 
-    loadSection2() {
-        let section = new Section2();
-        for (const [key] of Object.entries(section)) {
-            this.setObject(key);
-            for (let i = 0; i < section[key].length; i++) {
-                let object = section[key][i];
-                this[key].push(object);
-            }
-        }
+    // jsdoc
+    getObject(section, key, i) {
+        return section[key][i];
     }
 
 
-    loadSection3() {
-        let section = new Section3();
-        for (const [key] of Object.entries(section)) {
-            this.setObject(key);
-            for (let i = 0; i < section[key].length; i++) {
-                let object = section[key][i];
-                this[key].push(object);
-            }
-        }
+    // jsdoc
+    loadObject(t, key, object) {
+        this.setObjectX(t, object);
+        this[key].push(object);
     }
 
 
-    loadSection4() {
-        let section = new Section4();
-        for (const [key] of Object.entries(section)) {
-            this.setObject(key);
-            for (let i = 0; i < section[key].length; i++) {
-                let object = section[key][i];
-                this[key].push(object);
-            }
-        }
-    }
-
-
-    loadSection5() {
-        let section = new Section5();
-        for (const [key] of Object.entries(section)) {
-            this.setObject(key);
-            for (let i = 0; i < section[key].length; i++) {
-                let object = section[key][i];
-                this[key].push(object);
-            }
-        }
-    }
-
-
-    loadSection6() {
-        let section = new Section6();
-        for (const [key] of Object.entries(section)) {
-            this.setObject(key);
-            for (let i = 0; i < section[key].length; i++) {
-                let object = section[key][i];
-                this[key].push(object);
-            }
-        }
-    }
-
-
-    loadSection7() {
-        let section = new Section7();
-        for (const [key] of Object.entries(section)) {
-            this.setObject(key);
-            for (let i = 0; i < section[key].length; i++) {
-                let object = section[key][i];
-                this[key].push(object);
-            }
-        }
-    }
-
-
-    loadSection8() {
-        let section = new Section8();
-        for (const [key] of Object.entries(section)) {
-            this.setObject(key);
-            for (let i = 0; i < section[key].length; i++) {
-                let object = section[key][i];
-                this[key].push(object);
-            }
-        }
+    // jsdoc
+    setObjectX(t, object) {
+        object.x = object.x * 64 + t * canvas.width;    // to edit
     }
 }
