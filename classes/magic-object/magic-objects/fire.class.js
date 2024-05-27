601b1a321335a3677b30cf38d7f0c87e09cdd349
Your Name <you@example.com>
2024-05-27 21:25:56 +0200

    set fire move values

--- classes/magic-object/magic-objects/fire.class.js
+++ classes/magic-object/magic-objects/fire.class.js
@@ -1,6 +1,6 @@
 class Fire extends MagicObject {
     radDispl = 248;
-    speedXY = { s: 128, yUp: 112.8, yDown: -16.8 };    // to set!
+    speedXY = { x: 128, y: 64 };    // to set!
     pages = { move: [0, 3], collided: [3, 9], epilog: [9, 10] };
     bodyXY = { xLeft: 108, xCenter: 124, xRight: 140, yTop: 120, yCenter: 128, yBottom: 136 };
 
@@ -8,8 +8,11 @@ class Fire extends MagicObject {
     // jsdoc
     constructor(x, y, otherDirection) {
         super(source.fire, x, y);
+
         this.setMagic(otherDirection, 30, 'fire9');
-        this.setSpeed(this.speedXY.s);
+        this.startX = this.body.xLeft;
+        this.endX = this.startX - 376;
+        this.setSpeed(this.speedXY.x, 0, this.speedXY.y);
     }
 
 
@@ -18,6 +21,38 @@ class Fire extends MagicObject {
 
     // to edit!!!
     move() {
+        if (!isTrue(this.collided)) {
+            this.x -= this.speed;
 
+            if (isGreater(this.endX, this.body.xRight)) {
+                if (this.y + 128 < world.hero.body.yCenter) {
+                    if (this.y + 128 > world.hero.body.yCenter) {
+                        this.y = world.hero.body.yCenter - 128;
+                    } else {
+                        this.y += this.speedY;
+                    }
+                }
+
+
+                if (this.y + 128 > world.hero.body.yCenter) {
+                    if (this.y + 128 < world.hero.body.yCenter) {
+                        this.y = world.hero.body.yCenter - 128;
+                    } else {
+                        this.y -= this.speedY;
+                    }
+                }
+
+
+                console.log(this.y, this.body.yCenter, this.body.yCenter - this.y);
+            }
+        }
     }
+
+
+    // class Fire ...
+    // class Blade ...
+    // class MagicObject ...
+    // class Shaman ...
+    // './img' --> 'img' ...
+    // speedXY + pagesXY ...
 }
