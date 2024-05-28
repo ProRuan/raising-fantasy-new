1f221d66193c3ad0c00cdc05455ed181b5881f51
Your Name <you@example.com>
2024-05-28 11:16:15 +0200

    working on lightning move method

--- classes/magic-object/magic-objects/lightning.class.js
+++ classes/magic-object/magic-objects/lightning.class.js
@@ -1,5 +1,6 @@
 class Lightning extends MagicObject {
     radDispl = 260;
+    pages = { move: 2, collided: 8, epilog: 9 };
     bodyXY = { xLeft: 114, xCenter: 130, xRight: 146, yTop: 32, yCenter: 128, yBottom: 224 };
     loadXY = { xLeft: 120, xCenter: 128, xRight: 136, yTop: 180, yCenter: 128, yBottom: 226 };
     lightningXY = { xLeft: 114, xCenter: 130, xRight: 146, yTop: 32, yCenter: 128, yBottom: 224 };
@@ -7,20 +8,19 @@ class Lightning extends MagicObject {
 
     constructor(x, y, otherDirection) {
         super(source.lightning, x, y);
+        this.setMagic(otherDirection, 40, 'lightning8');
 
-        this.bodyXY = this.loadXY;
-        this.otherDirection = otherDirection;    // necessary?
+        this.searchStop = 5000 + getTime();
     }
 
 
-    // only for testing!!!
-    animate() {
-        this.img.src = this.flipBook[0];
+    move() {
+        if (isGreater(world.time, this.searchStop)) {
+            this.x = world.hero.body.xCenter - (this.body.xCenter - this.x);
+        }
     }
 
 
-
-    
     // class Lightning ...
     // class MagicObject ...
     // class Shaman ...
