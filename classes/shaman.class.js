36b6879100103955dc09f99fea351b56608570d4
Your Name <you@example.com>
2024-05-29 14:45:42 +0200

    cleaned class Shaman til loc 15

--- classes/shaman.class.js
+++ classes/shaman.class.js
@@ -1,21 +1,16 @@
 class Shaman extends Enemy {
     radDispl = 144;
-    spellCast = false;
     bodyXY = { xLeft: 56, xCenter: 72, xRight: 88, yTop: 104, yCenter: 154, yBottom: 204 };
-
-    weaponXY = { xLeft: 46, xRight: 94, yTop: 127, yBottom: 143 };    // to edit
+    weaponXY = { xLeft: 0, xRight: 0, yTop: 0, yBottom: 0 };
     bladeXY = { xLeft: -57, xRight: 220, yTop: -284, yBottom: 0 };
     fireXY = { xLeft: -20, xRight: 228, yTop: -297, yBottom: 0 };
+    lightningXY = { xLeft: -130, xRight: 228, yTop: -14 + 96, yBottom: 0 };
+    spellCast = false;
 
-    loadXY = { xLeft: -130, xRight: 228, yTop: -14 + 96, yBottom: 0 };   // to edit
-    lightningXY = { xLeft: -130, xRight: 228, yTop: -14, yBottom: 0 };
 
-
+    // jsdoc
     constructor(x, y) {
         super(source.shaman, x, y);
-
-        // this.weaponXY = this.bladeXY;
-        // this.setLightning();
         this.setEnemy(90, 64, 'cast');
     }
 
@@ -46,6 +41,7 @@ class Shaman extends Enemy {
             // this.setBlade();    // finished
             // this.setFire();    // finished
         }
+        // console.log(world.hero.body.yBottom, this.magic.body.yBottom);
     }
 
 
@@ -68,10 +64,10 @@ class Shaman extends Enemy {
 
 
     setLightning() {
-        let x = (world.hero.body.xCenter + this.loadXY.xLeft) / 64;
-        let y = (canvas.height - world.hero.yBottom + this.loadXY.yTop) / 64;
-        // let x = (world.hero.body.xCenter + this.lightningXY.xLeft) / 64;
-        // let y = (canvas.height - world.hero.yBottom + this.lightningXY.yTop) / 64;
+        this.weaponXY = this.lightningXY;
+
+        let x = (world.hero.body.xCenter + this.weapon.xLeft) / 64;
+        let y = (canvas.height - world.hero.yBottom + this.weapon.yTop) / 64;
         this.magic = new Lightning(x, y, this.otherDirection);
 
         // console.log(world.hero.body.xCenter, this.magic.body.xCenter, world.hero.body.yBottom, this.magic.body.yBottom);
@@ -92,7 +88,6 @@ class Shaman extends Enemy {
 
 
 
-    // class MagicObject ...
     // class Shaman ...
     // './img' --> 'img' ...
     // magic soung (cast + hit) ...
