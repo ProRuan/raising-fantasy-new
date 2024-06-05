ad235cacebc8899d5bdfe18fbf6cbf9fefc88d69
Your Name <you@example.com>
2024-06-05 12:52:32 +0200

    added quest text

--- classes/quest-roll.class.js
+++ classes/quest-roll.class.js
@@ -1,13 +1,9 @@
 class QuestRoll extends DrawableObject {
     opened = false;
-
-
-    xText = { left: 64, right: 80 };
-    yHeadline = { a: 100, b: 276 };
-    yScore = { best: 136, last: 312 };
-    yItem = { coins: 0, leaves: 36, time: 72 };
-    yVolume = { music: 144, sound: 192 };
-    volumeButtons = ['lowMusicButton', 'highMusicButton', 'lowSoundButton', 'highSoundButton'];
+    yHeadline = 160;
+    yStory = { first: 208, second: 238 };
+    ySubheadline = 298;
+    yChallenge = { first: 333, second: 363, third: 393 };
 
 
     // jsdoc
@@ -18,7 +14,18 @@ class QuestRoll extends DrawableObject {
 
 
     // jsdoc
-    show() {
+    get xHeadline() {
+        return this.x + this.width / 2;
+    }
+
+
+    // jsdoc
+    get xText() {
+        return this.x + 40;
+    }
+
+
+    show() {    // double code?
         setInterval(() => {
             this.showButtons();
         }, 1000 / 60);
@@ -36,21 +43,62 @@ class QuestRoll extends DrawableObject {
     }
 
 
+    // jsdoc
     setButtons(logical) {
         this.setReachable('newGameButton', logical);
         this.setReachable('questButton', logical);
-        (!logical) ? this.setReachable('coinButton', true) : this.setReachable('coinButton', false);
+        this.setCoinButton(logical);
     }
 
 
-    // jsdoc
     setReachable(key, value) {    // double code!!! (move)
         world[key].reachable = value;
     }
 
 
+    // jsdoc
+    setCoinButton(logical) {
+        logical = (!logical) ? true : false;
+        this.setReachable('coinButton', logical);
+    }
+
+
+    // jsdoc
     drawQuest() {
-        // world.setText('bold 28px Arial', 'center', 'black');
-        // console.log('quest roll content');
+        this.drawHeadline();
+        this.drawStory();
+        this.drawSubheadline();
+        this.drawChallenges();
+    }
+
+
+    // jsdoc
+    drawHeadline() {
+        world.setText('bold 28px Arial', 'center', 'black');
+        world.drawText('Quest', this.xHeadline, this.yHeadline);
+    }
+
+
+    // jsdoc
+    drawStory() {
+        world.setText('20px Arial', 'left', 'black');
+        world.drawText('Play as a knight and', this.xText, this.yStory.first);
+        world.drawText('explore a new world.', this.xText, this.yStory.second);
+    }
+
+
+    // jsdoc
+    drawSubheadline() {
+        world.setText('bold 20px Arial', 'left', 'black');
+        world.drawText('Challenges', this.xText, this.ySubheadline);
+    }
+
+
+    // jsdoc
+    drawChallenges() {
+        world.setText('20px Arial', 'left', 'black');
+        world.drawText('1. Collect all coins.', this.xText, this.yChallenge.first);
+        world.drawText('2. Collect all leaves.', this.xText, this.yChallenge.second);
+        world.drawText('3. Defeat the endboss.', this.xText, this.yChallenge.third);
     }
 }
