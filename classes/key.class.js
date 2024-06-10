26afed4e643e932e15ab4bf6975f93c93b546d61
Your Name <you@example.com>
2024-06-10 10:07:35 +0200

    cleaned class Key

--- classes/key.class.js
+++ classes/key.class.js
@@ -1,14 +1,19 @@
 class Key {
-    name;
-    code;
     keydown = false;
+    doubleClick = false;
     timeStamp = new Date().getTime();
-    lastKeyUp = 0;    // in use?
-    doubleClick = false;    // in use?
+    lastKeyUp = 0;
     locked = false;
 
 
+    // jsdoc
     constructor(name, code) {
+        this.setKey(name, code);
+    }
+
+
+    // jsdoc
+    setKey(name, code) {
         this.name = name;
         this.code = code;
     }
