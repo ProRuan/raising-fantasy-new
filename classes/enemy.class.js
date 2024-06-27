e3166bb566b8bf7de0bd8e6d9af2e94b832e11ac
Your Name <you@example.com>
2024-06-27 11:11:20 +0200

    commented class Enemy

--- classes/enemy.class.js
+++ classes/enemy.class.js
@@ -1,3 +1,7 @@
+/**
+ * Represents an enemy.
+ * @extends MoveableObject
+ */
 class Enemy extends MoveableObject {
     otherDirection = true;
     chapter = 'idle';
@@ -5,22 +9,36 @@ class Enemy extends MoveableObject {
     ableToFight = true;
 
 
-    // jsdoc
+    /**
+     * Creates an enemy.
+     * @param {object} source - The source object.
+     * @param {number} x - The x value.
+     * @param {number} y - The y value.
+     */
     constructor(source, x, y) {
         super(source, x * UNIT, y * UNIT);
         this.setAnimation(source);
-        this.setGrowl(source);
+        this.setEnemySounds(source);
     }
 
 
-    setGrowl(source) {    // rename!!!
+    /**
+     * Sets the sounds of the enemy.
+     * @param {object} source - The source object.
+     */
+    setEnemySounds(source) {
         this.growl = source.growl;
         this.weaponImpact = source.weaponImpact;
         this.armorHit = source.armorHit;
     }
 
 
-    // jsdoc
+    /**
+     * Sets the enemy.
+     * @param {number} hp - The value of the hp.
+     * @param {number} speed - The value of the speed.
+     * @param {string} method - The key of the method to apply.
+     */
     setEnemy(hp, speed, method) {
         this.setStateValues(hp, speed);
         this.setAct(method);
@@ -28,7 +46,11 @@ class Enemy extends MoveableObject {
     }
 
 
-    // jsdoc
+    /**
+     * Sets the state values.
+     * @param {number} hp - The value of the hp.
+     * @param {number} speed - The value of the speed.
+     */
     setStateValues(hp, speed) {
         this.hpMax = hp;
         this.hp = hp;
@@ -36,20 +58,27 @@ class Enemy extends MoveableObject {
     }
 
 
-    // jsdoc
+    /**
+     * Sets the act of the enemy.
+     * @param {string} method - The key of the method to apply.
+     */
     setAct(method) {
         this.act = this[method];
     }
 
 
-    // jsdoc
+    /**
+     * Animates the enemy.
+     */
     animate() {
         this.setPauseableInterval(() => this.live(), 1000 / 60);
         this.setPauseableInterval(() => this.playAnimation(), 100);
     }
 
 
-    // jsdoc
+    /**
+     * Controls the habit of the enemy.
+     */
     live() {
         this.passAway();
         this.hurt();
@@ -60,7 +89,9 @@ class Enemy extends MoveableObject {
     }
 
 
-    // jsdoc
+    /**
+     * Ends the life of the enemy.
+     */
     passAway() {
         if (this.isEpilog() && isUndefined(this.dead)) {
             this.dead = true;
@@ -69,7 +100,9 @@ class Enemy extends MoveableObject {
     }
 
 
-    // jsdoc
+    /**
+     * Lets the enemy growl terminally.
+     */
     growlTerminally() {
         this.playSound(this.growl);
         setTimeout(() => this.playSound(this.growl), 100);
@@ -77,25 +110,37 @@ class Enemy extends MoveableObject {
     }
 
 
-    // jsdoc
+    /**
+     * Verifies, if the epilog animation is to play.
+     * @returns {boolean} - A boolean value.
+     */
     isEpilog() {
         return this.isDeath() && this.isEpilogImage();
     }
 
 
-    // jsdoc
+    /**
+     * Verifies, if the death animation is to play.
+     * @returns {boolean} - A boolean value.
+     */
     isDeath() {
         return !isGreater(0, this.hp);
     }
 
 
-    // jsdoc
+    /**
+     * Verifies, if the epilog image is reached.
+     * @returns {boolean} - A boolean value.
+     */
     isEpilogImage() {
         let fileName = 'death' + this.flipBook.death.length;
         return this.isImage(fileName);
     }
 
-    // jsdoc
+
+    /**
+     * Applies the hurt.
+     */
     hurt() {
         if (this.isHurt()) {
             if (this.isHeroWeapon()) {
@@ -105,13 +150,19 @@ class Enemy extends MoveableObject {
     }
 
 
-    // jsdoc
+    /**
+     * Verifies, if the hurt animation is to play.
+     * @returns {boolean} - A boolean value.
+     */
     isHurt() {
         return world.hero.isAttack() && world.hero.isBattle(this);
     }
 
 
-    // jsdoc
+    /**
+     * Verifies, if the enemy collides with a weapon.
+     * @returns {boolean} - A boolean value.
+     */
     isHeroWeapon() {
         let runAttack = world.hero.isImage('run_attack4');
         let walkAttack = world.hero.isImage('walk_attack4');
@@ -120,18 +171,23 @@ class Enemy extends MoveableObject {
     }
 
 
+    /**
+     * Applies the weapon hit.
+     */
     applyWeaponHit() {
         if (this.hitTime && isGreater(this.hitTime, world.time)) {
             this.hp -= world.hero.weaponDamage;
             this.playSound(this.weaponImpact);
             delete this.hitTime;
         } else if (!this.hitTime) {
-            this.hitTime = world.time + 100;
+            this.hitTime = getSum(world.time, 100);
         }
     }
 
 
-    // jsdoc
+    /**
+     * Applies the attack.
+     */
     attack() {
         if (this.isImage(this.damage.trigger) && isGreater(this.damage.time, world.time)) {
             this.applyDamage(this.damage.value);
@@ -141,7 +197,10 @@ class Enemy extends MoveableObject {
     }
 
 
-    // jsdoc
+    /**
+     * Applies the damage of the enemy attack.
+     * @param {number} damage - The value of the damage.
+     */
     applyDamage(damage) {
         let hpPoints = world.hero.hpPoints;
         let diff = getSum(hpPoints.length, -damage);
@@ -149,7 +208,9 @@ class Enemy extends MoveableObject {
     }
 
 
-    // jsdoc
+    /**
+     * Plays the hit sound of the hero.
+     */
     playHeroHitSound() {
         if (!(this instanceof Spider)) {
             this.playSound(this.armorHit);
@@ -157,7 +218,12 @@ class Enemy extends MoveableObject {
     }
 
 
-    // jsdoc
+    /**
+     * Sets the hp of the hero.
+     * @param {number} damage - The value of the damage.
+     * @param {number} hpPoints - The value of the hpPoints.
+     * @param {number} diff - The difference of the hp values.
+     */
     setHeroHp(damage, hpPoints, diff) {
         if (isGreater(diff, 0)) {
             let rest = hpPoints.length;
@@ -169,38 +235,54 @@ class Enemy extends MoveableObject {
     }
 
 
-    // jsdoc
+    /**
+     * Updates the time of the damage.
+     */
     updateDamageTime() {
         let delay = this.flipBook.attack.length * 100;
         this.damage.time = delay + getTime();
     }
 
 
-    // jsdoc
+    /**
+     * Verifies, if the attack animation is to play.
+     * @returns {boolean} - A boolean value.
+     */
     isAttack() {
         return this.isBattle(world.hero) && !this.isDeath() && !this.isHurt() && !world.hero.isDeath();
     }
 
 
-    // jsdoc
+    /**
+     * Verifies, if the idle animation is to play.
+     * @returns {boolean} - A boolean value.
+     */
     isIdle() {
         return true;
     }
 
 
-    // jsdoc
+    /**
+     * Verifies, if the enemy is peaceful.
+     * @returns {booelan} - A boolean value.
+     */
     isPeaceful() {
         return this.isFine() && !this.isAttack();
     }
 
 
-    // jsdoc
+    /**
+     * Verifies, if the enemy is fine.
+     * @returns {boolean} - A boolean value.
+     */
     isFine() {
         return !(this.isDeath() || this.isHurt());
     }
 
 
-    // jsdoc
+    /**
+     * Plays the animation.
+     */
     playAnimation() {
         super.playAnimation(this.flipBook[this.chapter]);
     }
