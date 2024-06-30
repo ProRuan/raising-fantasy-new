7e8a63d07dc7d9a92d7369d5efc9ac82938b0d50
Your Name <you@example.com>
2024-06-30 23:21:34 +0200

    cleaned game.js til loc 362

--- js/global.js
+++ js/global.js
@@ -40,10 +40,41 @@ function getTime() {
 }
 
 
+// jsdoc
+function isUndefined(value) {
+    return value === undefined;
+}
+
 
 // jsdoc
 function isTrue(value) {
     return (value == true) ? true : false;
+}
+
+
+// jsdoc
+function isCollided(a, b) {
+    return isCollidedX(a, b) && isCollidedY(a, b);
+}
+
+
+// jsdoc
+function isCollidedX(a, b) {
+    let aLeft = isIncluded(b.xLeft, a.xLeft, b.xRight);
+    let bLeft = isIncluded(a.xLeft, b.xLeft, a.xRight);
+    let aRight = isIncluded(b.xLeft, a.xRight, b.xRight);
+    let bRight = isIncluded(a.xLeft, b.xRight, a.xRight);
+    return (aLeft || bLeft) || (aRight || bRight);
+}
+
+
+// jsdoc
+function isCollidedY(a, b) {
+    let aTop = isIncluded(b.yTop, a.yTop, b.yBottom);
+    let bTop = isIncluded(a.yTop, b.yTop, a.yBottom);
+    let aBottom = isIncluded(b.yTop, a.yBottom, b.yBottom);
+    let bBottom = isIncluded(a.yTop, b.yBottom, a.yBottom);
+    return (aTop || bTop) || (aBottom || bBottom);
 }
 
 
@@ -64,6 +95,57 @@ function isIncluded(a, b, c) {
 // jsdoc
 function isGreater(a, b, tolerant) {
     return (!tolerant) ? a < b : a <= b;
+}
+
+
+// jsdoc
+function formatSplitWord(text) {
+    if (text.includes('_')) {
+        let formattedText = getFormattedSplitWord(text);
+        return formattedText;
+    } else {
+        return text;
+    }
+}
+
+
+// jsdoc
+function getFormattedSplitWord(text) {
+    text = text.split('_');
+    let firstWord = text[0];
+    let secondWord = formatInitial(text[1], 'toUpperCase');
+    return firstWord + secondWord;
+}
+
+
+// jsdoc
+function getLastElement(array) {
+    return array[array.length - 1];
+}
+
+
+// jsdoc
+function getRandomNumber(max, dev) {
+    return max - Math.round(Math.random() * dev);
+}
+
+
+// jsdoc
+function getSum(summandA, summandB) {
+    return summandA + summandB;
+}
+
+
+// jsdoc
+function getVerifiedValue(valueA, valueB) {
+    return isGreater(valueA, valueB) ? valueA : valueB;
+}
+
+
+// jsdoc
+function isOnTime(current, last, value) {
+    let timeDiff = current - last;
+    return isGreater(value, timeDiff);
 }
 
 
